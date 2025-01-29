4ff709336643f6dea52d1f8f67d10a64a100ccd2
H-Janath <janathhma@gmail.com>
2025-01-30 04:32:56 +0530

    Add authentication context, navbar, and sidebar components; implement login functionality with backend integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Login from './pages/Login'
-
+import { ToastContainer, toast } from 'react-toastify';
+import { AdminContext } from './context/AdminContext';
+import Navbar from './component/Navbar';
+import Sidebar from './component/Sidebar'
 const App = () => {
-  return (
-    <div>
-      <Login/>
+
+  const {aToken} = useContext(AdminContext)
+
+  return aToken?(
+    <div className='bg-[£F8F9FD]'>
+     
+      <ToastContainer/>
+      <Navbar/>
+      <div className='flex items-start '>
+        <Sidebar/>
+        
+      </div>
     </div>
+  ):(
+    <>
+     <Login/>
+     <ToastContainer/>
+    </>
   )
 }
 
