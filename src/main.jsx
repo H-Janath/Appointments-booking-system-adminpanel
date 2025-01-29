f297f8add9bcc051c0ef36b9891e00f6b942a218
H-Janath <janathhma@gmail.com>
2025-01-29 17:45:51 +0530

    Add context providers and login page component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowerRouter} from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
-  <BrowerRouter>
-    <App />
-    </BrowerRouter>
-)
+import AdminContextProvider from "./context/AdminContext.jsx";
+import DoctorContextProvide from './context/DoctorContext.jsx'
+import AppContextProvider from './context/AppContext.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AdminContextProvider>
+      <DoctorContextProvide>
+        <AppContextProvider>
+        <App />
+        </AppContextProvider>
+     
+      </DoctorContextProvide>
+    
+    </AdminContextProvider>
+  </BrowserRouter>
+);
